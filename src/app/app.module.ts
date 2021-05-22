21371feea51118c568be679c1d4bc456269a556e
SnehaJoglekar123 <snehaj.pagoanalytics@gmail.com>
2021-05-22 18:39:16 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,31 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';  //jack
+import {NgMultiSelectDropDownModule} from 'ng-multiselect-dropdown';
+
+// import {AngularFireModule} from '@angular/fire';
+// import {AngularDatabaseModule} from '@angular/fire/Database';
+
+// import { environment } from '../environments/environment';
+// import {AngylarFirestoreModule} from '@angular/fire/firestore;'
+
+// import {CrudService} from './service/crud.service';
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgMultiSelectDropDownModule.forRoot(),
+
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireDatabaseModule,
+    // AngularFirestoreModule
+      
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,22 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
+// FIREBASE CONNECTION
+
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+
+    apiKey: "AIzaSyBnBofgYqZJt6YOfqq_gIb8QJVEF-aotCo",
+    authDomain: "blogdata-1b879.firebaseapp.com",
+    projectId: "blogdata-1b879",
+    storageBucket: "blogdata-1b879.appspot.com",
+    messagingSenderId: "1009705917758",
+    appId: "1:1009705917758:web:793ffa099db1386de2606f",
+    measurementId: "G-W0SCLXEP05"
+
+  }
 };
 
 /*
